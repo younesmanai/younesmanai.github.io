2115d1ccef8af2191d12314ba6dd49e41ba84aaa
younesmanai <83230115+younesmanai@users.noreply.github.com>
2021-05-10 01:08:12 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+const box1 = document.querySelector(".box-container .box1");
+const box2 = document.querySelector(".box-container .box2");
+const box3 = document.querySelector(".box-container .box3");
+
+// SCREEN //
+const contentScreen = document.querySelector(".box-content-screen");
+const contentScreenParagraphe = document.querySelector(".box-content-screen p");
+
+const contentScreen2 = document.querySelector(".box-content-screen2");
+const contentScreenParagraphe2 = document.querySelector(".box-content-screen2 p");
+
+const contentScreen3 = document.querySelector(".box-content-screen3");
+const contentScreenParagraphe3 = document.querySelector(".box-content-screen3 p");
+
+
+box1.addEventListener("click", () => {
+	gsap.to(box1, {duration: 0.5, opacity: 0});
+	gsap.to(box2, {duration: 0.5, opacity: 0});
+	gsap.to(box3, {duration: 0.5, opacity: 0});
+
+	gsap.to(contentScreen, {duration: 0.5, scale: 1});
+
+
+
+			contentScreen.addEventListener("click", () => {
+	gsap.to(box1, {duration: 0.5, opacity: 1});
+	gsap.to(box2, {duration: 0.5, opacity: 1});
+	gsap.to(box3, {duration: 0.5, opacity: 1});
+
+	gsap.to(contentScreen, {duration: 0.5, scale: 0});
+
+
+});
+
+
+});
+
+box2.addEventListener("click", () => {
+	gsap.to(box2, {duration: 0.5, opacity: 0});
+	gsap.to(box1, {duration: 0.5, opacity: 0});
+	gsap.to(box3, {duration: 0.5, opacity: 0});
+
+	gsap.to(contentScreen2, {duration: 0.5, scale: 1});
+
+
+
+			contentScreen2.addEventListener("click", () => {
+	gsap.to(box1, {duration: 0.5, opacity: 1});
+	gsap.to(box2, {duration: 0.5, opacity: 1});
+	gsap.to(box3, {duration: 0.5, opacity: 1});
+
+	gsap.to(contentScreen2, {duration: 0.5, scale: 0});
+
+
+});
+
+
+});
+
+box3.addEventListener("click", () => {
+	gsap.to(box3, {duration: 0.5, opacity: 0});
+	gsap.to(box1, {duration: 0.5, opacity: 0});
+	gsap.to(box2, {duration: 0.5, opacity: 0});
+
+	gsap.to(contentScreen3, {duration: 0.5, scale: 1});
+
+
+
+			contentScreen3.addEventListener("click", () => {
+	gsap.to(box1, {duration: 0.5, opacity: 1});
+	gsap.to(box2, {duration: 0.5, opacity: 1});
+	gsap.to(box3, {duration: 0.5, opacity: 1});
+
+	gsap.to(contentScreen3, {duration: 0.5, scale: 0});
+
+
+});
+
+
+});
